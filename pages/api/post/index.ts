6a93c217b621bdd0cef1f8bf2c8dfcbcb5954a9b
SergioPeralta22 <seperalta88@gmail.com>
2022-07-20 12:09:03 -0300

    conection complete

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,12 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { allPostsQuery } from '../../../utils/queries';
 
-type Data = {
-    name: string;
-};
-
-export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Data>
-) {
-    res.status(200).json({ name: 'John Doe' });
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'GET') {
+        const query = allPostsQuery();
+    }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from 'next';
 import axios from 'axios';
+
 const Home: NextPage = () => {
     return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+};
+
+export const getServerSideProps = async () => {
+    const res = await axios.get(`http://localhost:3000/api/post`);
+    console.log(res.data.name);
+    return {
+        props: {},
+    };
 };
 
 export default Home;
